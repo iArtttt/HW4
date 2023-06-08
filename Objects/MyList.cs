b3d036f05ba0d2701f79001bf6b23ac0cf8e90ba
Artur <volk19011@gmail.com>
2023-06-09 00:30:15 +0300

    Create class MyList

---
+++ Objects/MyList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    internal class MyList
+    {
+        private uint _count = 0;
+        public int Count { get { return (int)_count; } }
+        public uint Capasity { get; private set; } = 10;
+        public object[] Arr; 
+        public MyList() 
+        {
+            Arr = new object[Capasity];
+        } 
+        public MyList(uint startCapasity) 
+        {
+            Capasity = startCapasity;
+            Arr = new object[Capasity];
+        }
+        public void Add(object item)
+        {
+            if (Count + 1 > Capasity)
+            {
+                Capasity *= 2;
+                Arr = new object[Capasity];
+            }
+            Arr[_count++] = item;
+        }
+        public void Insert(int index, object item)
+        {
+            Arr[index] = item;
+        }
+        public int IndexOf(object item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Arr[i] == item)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public bool Contains(object item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Arr[i] == item)
+                    return true;
+            }
+            return false;
+        }
+        public void Remove(object obj)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Arr[i] == obj)
+                {
+                    Arr[i] = null;
+                }
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            Arr[index] = null;
+        }
+        public void Clear()
+        {
+            Arr = null;
+        }
+    }
+}
---
+++ Objects/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList myList = new MyList();
+
+            myList.Add(1);
+            myList.Add("sds");
+            myList.Add("aa");
+            myList.Add('s');
+            myList.Add(55.156m);
+            
+            int c = myList.Count;
+
+            int indexOf = myList.IndexOf('s');
+            int indexOf1 = myList.IndexOf(1);
+            //myList.RemoveAt(indexOf);
+
+            myList.Add(7789.66f);
+            myList.Add("qdw");
+            myList.Add("bmkkkf");
+            myList.Add(23);
+
+            bool b = myList.Contains(1);
+            myList.Add(83);
+            myList.Add(546);    
+            int s = myList.Count;
+        }
+    }
+}
